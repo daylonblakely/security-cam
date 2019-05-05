e869b5476951b4f0f50ab94ff153b8ef68c9a0a5
Tommy <tmargolis@ltu.edu>
2019-05-05 19:35:40 -0400

    Final project presented in class.

---
+++ Footage.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.File;
+
+/*Footage object used for handling the saving of video
+ * data. Stores an array of files in a linked list.*/
+public class Footage {
+	//Local split video container of files
+	private LinkedList<File[]> splitVids = new LinkedList<File[]>();
+	
+	//Constructor initialized with file array
+	public Footage(File []day) {
+		splitVids.add(day);
+	}
+	
+	//Constructor with no initialization
+	public Footage() {
+		File[] day = new File[23];
+		splitVids.add(day);
+	}
+	
+	//Insert an entire file array(story) given via parameter
+	void insert_story(File [] story) {
+		splitVids.add(story);
+	}
+	
+	//Insert a single segment of video into a file array at the next available position
+	void insert_segment(File segment, int index) {
+		File[] selectedStory = splitVids.get(index);
+		for (int i = 0; i < selectedStory.length; i++) {
+			if(selectedStory[i]==null) 
+				selectedStory[i] = segment;
+			else if(selectedStory[i]!=null && selectedStory.length == i-1)
+				System.err.print("Selected story is full");
+		}
+		splitVids.remove(index);
+		splitVids.add(selectedStory);
+	}
+	
+	//Return the segment of video in the story at parameters 
+	File get_segment(int story, int index) {
+		return splitVids.get(story)[index];
+	}
+}
--- SplitVideo.java
+++ SplitVideo.java
@@ -1,19 +1,64 @@
-import java.io.*;
-import java.nio.file.Files;
-
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+/**
+ * 
+ * @author uday.p
+ *
+ */
 public class SplitVideo {
+    public static void main(String[] args) {
+        try {
+            File file = new File("new_vid.mp4");//File read from Source folder to Split.
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		File fileIn = new File("new_vid.mp4");
-		
-		System.out.println(fileIn.length());
-		byte[] fileContent = Files.readAllBytes(fileIn.toPath());
-		
-		File fileOut = new File("vid_segment.mp4");
-		FileOutputStream fos = new FileOutputStream(fileOut);
-		fos.write(fileContent,fileContent.length/4,(3*fileContent.length/4));
-		fos.close();
-	}
+            
+            if (file.exists()) {
+            	
+            String videoFileName = file.getName().substring(0, file.getName().lastIndexOf(".")); // Name of the videoFile without extension
+            File splitFile = new File("C:/Users/support/java-docs/Security_Cam/"+ videoFileName);//Destination folder to save.
+            if (!splitFile.exists()) {
+                splitFile.mkdirs();
+                System.out.println("Directory Created -> "+ splitFile.getAbsolutePath());
+            }
+
+            int i = 01;// Files count starts from 1
+            InputStream inputStream = new FileInputStream(file);
+            String videoFile = splitFile.getAbsolutePath() +"/"+ String.format("%02d", i) +"_"+ file.getName();// Location to save the files which are Split from the original file.
+            OutputStream outputStream = new FileOutputStream(videoFile);
+            System.out.println("File Created Location: "+ videoFile);
+            int totalPartsToSplit = 24;// Total files to split.
+            
+            File[] initial = new File[totalPartsToSplit];
+            int splitSize = inputStream.available() / totalPartsToSplit;
+            int streamSize = 0;
+            int read = 0;
+            while ((read = inputStream.read()) != -1) {
+
+                if (splitSize == streamSize) {
+                    if (i != totalPartsToSplit) {
+                        i++;
+                        String fileCount = String.format("%02d", i); // output will be 1 is 01, 2 is 02
+                        videoFile = splitFile.getAbsolutePath() +"/"+ fileCount +"_"+ file.getName();
+                        initial[i] = splitFile;
+                        outputStream = new FileOutputStream(videoFile);
+                        System.out.println("File Created Location: "+ videoFile);
+                        streamSize = 0;
+                    }
+                }
+                outputStream.write(read);
+                streamSize++;
+            }
+            Footage data = new Footage(initial);
+            inputStream.close();
+            outputStream.close();
+            System.out.println("Total files Split ->"+ totalPartsToSplit);
+        } else {
+            System.err.println(file.getAbsolutePath() +" File Not Found.");
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
 }
